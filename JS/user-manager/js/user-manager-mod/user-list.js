51117f1c7324b0e6d94235060e320d0be4972ea9
Issayah <copseyi@students.westerntc.edu>
2021-02-03 10:53:54 -0600

    pastehack

--- JS/user-manager/js/user-manager-mod/user-list.js
+++ JS/user-manager/js/user-manager-mod/user-list.js
@@ -16,6 +16,27 @@ class UserList{
 
     //Methods
     render(){
-        
+        this.container.innerHTML = "";
+        if (this.users && this.users.length > 0) {
+            this.ul = document.createElement("ul");
+            this.users.forEach(user => {
+                var li = document.createElement("li");
+                li.innerHTML = `${user.firstName} ${user.lastName}`;
+                li.setAttribute("userId", user.id);
+                this.ul.appendChild(li);
+            });
+            this.container.appendChild(this.ul);
+            this.ul.addEventListener("click", evt => {
+                var selectedUserId = evt.target.getAttribute("userId");
+                //alert(selectedUserId);
+                if (selectedUserId) {
+                    // we should create a triggerEvent() method to do this
+                    this.selectedListeners.forEach(listener => {
+                        listener(selectedUserId);
+                    });
+                }
+            });
+        }
+
     }
 }
