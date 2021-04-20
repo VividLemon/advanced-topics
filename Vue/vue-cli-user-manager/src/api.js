8933469e3213e5a35b5353c229ae96a203fb6866
Issayah <copseyi@students.westerntc.edu>
2021-04-19 23:12:59 -0500

    api and final changes

--- Vue/vue-cli-user-manager/src/api.js
+++ Vue/vue-cli-user-manager/src/api.js
@@ -1,8 +1,35 @@
 import axios from 'axios'
 
+const devUrl = "http://localhost/api/";
+const liveUrl = "https://www.wtc-web.com/api/"; // PUT YOUR DOMAIN NAME HERE watch out for the www
+var url;
+
+if(location.hostname == "localhost"){
+    url = devUrl;
+}else{
+    url = liveUrl;
+    // we want to make sure we are using https on the live server
+    // I got this code from here: https://stackoverflow.com/questions/4723213/detect-http-or-https-then-force-https-in-javascript
+    if (location.protocol !== 'https:') {
+        location.replace(`https:${location.href.substring(location.protocol.length)}`);
+    }
+}
+
 const ax = axios.create({
-    baseURL: 'http://localhost/api/'
+    baseURL: url
 });
+// We'll get the session id (from the x-id response header) when a user logs in (see the login() method below), 
+// and send it in every request so that the server can keep the session going 
+var sessionId = "";
+
+// You can 'intercept' all requests made by ax, and add your own headers to the request
+// Here we're adding the x-id header so that the server can keep the session going
+ax.interceptors.request.use(
+    function(request) {
+        request.headers['x-id'] = sessionId;
+        return request;
+    }
+)
 
 const UserDataAccess = {
     getAllUsers(){
@@ -15,7 +42,21 @@ const UserDataAccess = {
         return ax.put("users/" + user.id, user).catch((error) => errorHandler("Error Inserting User:" + error));
     },
     login(email, password){
-        return ax.post("login/", {email,password});//.catch((error) => errorHandler("Error logging in" + error));
+        
+        return ax.post("login/", {email,password}).then(resp => {
+            
+            // When you log in on the server, it will send the x-id header, which is the session id
+            if(resp.headers['x-id']){
+                // we'll store the session id so that we can send it in every subsequent request
+                sessionId = resp.headers['x-id'];
+            }
+            
+            // make sure to return a Promise so that your code in the Login.vue
+            // can do it's then()
+            return new Promise((resolve, reject) => {
+                resolve(resp);
+            });
+        });
     },
     getUserById(id){
         return ax.get("users/" + id).catch((error) => errorHandler("Error getting user by id:" + error));
